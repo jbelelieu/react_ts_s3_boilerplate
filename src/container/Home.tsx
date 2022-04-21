2bf1c37e061faa5bfab6a63c8343b0ab4df7b71e
Jonathan Belelieu <jonnyobell@gmail.com>
2022-04-21 10:11:41 -0400

    Update the Home to coming soon!

--- src/container/Home.tsx
+++ src/container/Home.tsx
@@ -8,6 +8,7 @@ function Home() {
         <div className="section">
             <main>
                 <h2>Welcome to Pikle!</h2>
+                <p>The app is coming very soon!</p>
             </main>
             <nav>
                 {/* <Link to="/about">About</Link> */}
